3001450aa4e7979d15cbe2f4c7c500bdae868c63
Arkadio <arkadio918@gmail.com>
2024-05-25 00:13:43 +0200

    Update Mute.cookie.load.js

--- Mute.cookie.load.js
+++ Mute.cookie.load.js
@@ -1,8 +1,8 @@
-const readyCheck = setInterval(() => {
-  const Game = unsafeWindow.Game;
-
-  if (typeof Game !== 'undefined' && typeof Game.ready !== 'undefined' && Game.ready) {
-    Game.LoadMod('https://github.com/Arkadio918/Mute-cookie/blob/main/Mute_cookie.js');
-    clearInterval(readyCheck);
-  }
-}, 1000);
+javascript:(function() {
+    var checkReady = setInterval(function() {
+        if (typeof Game.ready !== 'undefined' && Game.ready) {
+            Game.LoadMod('https://github.com/Arkadio918/Mute-cookie/blob/main/Mute_cookie.js');
+            clearInterval(checkReady);
+        }
+    }, 1000);
+}());
