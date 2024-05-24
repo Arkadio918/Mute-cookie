80c1e4b52df080df532d2f1c99fcae888495abf9
Arkadio <arkadio918@gmail.com>
2024-05-25 00:23:31 +0200

    Update Mute_cookie.js

--- Mute_cookie.js
+++ Mute_cookie.js
@@ -1,1 +1,14 @@
+console.log('[==Starting==]');
 Game.playCookieClickSound=function(){return};
+console.log('[==ending==]');
+var autoClickButton = document.createElement("button");
+autoClickButton.innerText = "Autoclicker: " + buttonStateText[doLoop];
+autoClickButton.setAttribute("style", buttonCSS);
+ 
+autoClickButton.onclick = function(){
+  doLoop = !doLoop;
+  autoClickButton.innerText = "Autoclicker: " + buttonStateText[doLoop];
+  clickLoop();
+  shimmerLoop();
+}
+console.log('[==ending PT 2==]');
